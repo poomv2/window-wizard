55f1415c1f11237aa00c6a401a798be863e80e5c
Steve Forbes <sonicpoom@gmail.com>
2017-03-14 01:23:06 +0000

    adding git ignore. adding automatic copyright date.

--- htdocs/includes/templates/footer.php
+++ htdocs/includes/templates/footer.php
@@ -5,7 +5,7 @@
       <a href="#" class="footer-facebook-icon"><img src="/img/svg/facebook-logo.svg" alt="Visit the Window Wizard Facebook page!"></a>
     </div>
     <div class="medium-6 medium-pull-6 columns">
-      <small>&copy; 2017 The Window Wizard Horsham | All Rights Reserved</small>
+      <small>&copy; <?php echo date("Y"); ?> The Window Wizard Horsham | All Rights Reserved</small>
     </div>
   </div>
 </footer>
